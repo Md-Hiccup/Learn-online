a4dcd9c1952f912bbdef427c8a1f6cbec2c6b175
Md-Hiccup <ask2md@gmail.com>
2018-07-02 18:46:36 +0530

    lip: understanding the IIFE

--- Nodejs/Basic JS/section-4/15-IIFEandSafe.js
+++ Nodejs/Basic JS/section-4/15-IIFEandSafe.js
@@ -1,7 +1,21 @@
+var greeting = "Hola ";
 
+// 1.  IIFE
 (function greet(name){
     var greeting = 'Hello ';
+    console.log(greeting + name);       // 1.   Hello John
+
+})('John');     // IIFE
+
+console.log(greeting);                  // 2.   Hola
+
+
+// 2.   IIFE
+(function greet( global, name){
+    var greeting = 'hello ';
+    global.greeting = "hello ";          // override the 'global' object 'greeting' variable from 'Hola' to 'Hello' 
     console.log(greeting + name);
 
-})('John');
+}(window, 'john'));                 // window is not working on node so open in the browser for checking the code
 
+console.log(greeting)
