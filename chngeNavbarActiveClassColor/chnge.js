fa39aa6365339810226bbe0123b6280d5e40aec6
hussain <ask2md@gmail.com>
2018-07-10 23:52:10 +0530

    extra stuff navbar

--- chngeNavbarActiveClassColor/chnge.js
+++ chngeNavbarActiveClassColor/chnge.js
@@ -2,33 +2,37 @@ $(document).ready(function () {
   $(document).on("scroll", onScroll);
   
   //smoothscroll
-  $('a[href^="#"]').on('click', function (e) {
-      e.preventDefault();
-      $(document).off("scroll");
+  // $('a[href^="#"]').on('click', function (e) {
+  //     e.preventDefault();
+  //     $(document).off("scroll");
       
-      $('a').each(function () {
-          $(this).removeClass('active');
-      })
-      $(this).addClass('active');
+  //     $('a').each(function () {
+  //         $(this).removeClass('active');
+  //     })
+  //     $(this).addClass('active');
     
-      var target = this.hash,
-          menu = target;
-      $target = $(target);
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top+2
-      }, 500, 'swing', function () {
-          window.location.hash = target;
-          $(document).on("scroll", onScroll);
-      });
-  });
+  //     var target = this.hash,
+  //         menu = target;
+  //     $target = $(target);
+  //     $('html, body').stop().animate({
+  //         'scrollTop': $target.offset().top+2
+  //     }, 500, 'swing', function () {
+  //         window.location.hash = target;
+  //         $(document).on("scroll", onScroll);
+  //     });
+  // });
 });
 
 function onScroll(event){
   var scrollPos = $(document).scrollTop();
   $('#menu-center a').each(function () {
       var currLink = $(this);
+      // console.log(currLink)
       var refElement = $(currLink.attr("href"));
-      console.log('refElement: ', refElement);
+      // console.log('refElement: ', refElement);
+      console.log(refElement.position().top ,"<", scrollPos,": ",refElement.position().top <= scrollPos )
+      console.log(refElement.position().top , "+", refElement.height(), ">" ,scrollPos,": ",refElement.position().top + refElement.height() > scrollPos)
+      console.log("----------------")
       if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
           $('#menu-center ul li a').removeClass("active");
           currLink.addClass("active");
