b8335c4906e68db80e58d51645b14a899cc4a474
Md-Hiccup <ask2md@gmail.com>
2018-07-11 17:22:43 +0530

    wip: done user register

---
+++ Nodejs/devconnector/client/src/store.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const initialState = {};
+
+const middleware = [thunk];
+/* eslint-disable no-underscore-dangle */
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware)),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+  )
+)
+/* eslint-enable */
+
+export default store;
