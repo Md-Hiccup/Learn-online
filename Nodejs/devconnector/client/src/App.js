140684f14894aaea9a2b8d0be9d6c0193f559b93
Md-Hiccup <ask2md@gmail.com>
2018-07-12 14:42:37 +0530

    wip: loading spinner & private route

--- Nodejs/devconnector/client/src/App.js
+++ Nodejs/devconnector/client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 import { Provider } from "react-redux";
 // Provider is react component which provides application with store(which holds state).
 import store from "./store";
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -14,6 +16,7 @@ import Landing from "./components/layout/Landing";
 
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
 
@@ -32,7 +35,7 @@ if (localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser());
     // ToDo: Clear current Profile
-
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = '/login';
   }
@@ -49,6 +52,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>  
             </div>
             <Footer />
           </div>
