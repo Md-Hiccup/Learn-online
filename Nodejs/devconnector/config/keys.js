41e23f1e20d7d066cf31992a19cf34bc11d86752
Md-Hiccup <ask2md@gmail.com>
2018-07-16 14:44:15 +0530

    wip: hosting on heroku

--- Nodejs/devconnector/config/keys.js
+++ Nodejs/devconnector/config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://jack:jack123@ds039261.mlab.com:39261/devconnector',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
